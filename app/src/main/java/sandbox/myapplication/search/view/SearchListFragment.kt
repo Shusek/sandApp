8b4fd06e8edd6dd36f37f2b4f76d54d7797a6263
m.suska <m.suska@partner.samsung.com>
2018-09-03 14:58:20 +0200

    Add search icon to SearchListFragment

--- app/src/main/java/sandbox/myapplication/search/view/SearchListFragment.kt
+++ app/src/main/java/sandbox/myapplication/search/view/SearchListFragment.kt
@@ -1,15 +1,43 @@
 package sandbox.myapplication.search.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.SearchView
+import android.view.*
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.repository_list.*
 import sampleapp.istari.myapplication.R
+
 
 class SearchListFragment : DaggerFragment() {
 
+    private val listAdapter by lazy { RepositoriesAdapter {} }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.repository_list, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        repositoryList.adapter = listAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu.findItem(R.id.action_search).actionView as SearchView
+        searchItem.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return true
+            }
+
+        })
+
     }
 }
