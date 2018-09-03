ad345eae2906c155edb4239014f7e9ec3493179b
m.suska <m.suska@partner.samsung.com>
2018-09-03 16:45:56 +0200

    Connect toolbar to navigation graph

--- app/src/main/java/sandbox/myapplication/MainActivity.kt
+++ app/src/main/java/sandbox/myapplication/MainActivity.kt
@@ -2,12 +2,22 @@ package sandbox.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import sampleapp.istari.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(null, navController)
     }
 }
