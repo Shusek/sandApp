5ea58f2a30cecf534c3b884656aada9eb2430e08
m.suska <m.suska@partner.samsung.com>
2018-09-03 16:19:56 +0200

    Add kotlin json converter to retrofit instance

--- app/src/main/java/sandbox/myapplication/common/di/NetworkModule.kt
+++ app/src/main/java/sandbox/myapplication/common/di/NetworkModule.kt
@@ -1,10 +1,14 @@
 package sandbox.myapplication.common.di
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.stringBased
 import dagger.Module
 import dagger.Provides
+import kotlinx.serialization.json.JSON
 import okhttp3.Interceptor
+import okhttp3.MediaType
 import okhttp3.OkHttpClient
+import retrofit2.Converter
 import retrofit2.Retrofit
 import sandbox.myapplication.common.api.GithubApi
 import javax.inject.Singleton
@@ -16,39 +20,44 @@ class NetworkModule {
 
     @Provides
     @Singleton
-    fun providesOkHttpClient(interceptors:List<Interceptor>): OkHttpClient {
+    fun providesOkHttpClient(interceptor: Interceptor): OkHttpClient {
         val httpClientBuilder = OkHttpClient.Builder()
         return httpClientBuilder
-                .addInterceptors(interceptors)
+                .addInterceptor(interceptor)
                 .build()
     }
 
     @Provides
     @Singleton
-    fun providesInterceptors(): List<Interceptor> {
-      return listOf( Interceptor { chain ->
+    fun providesInterceptor(): Interceptor {
+        return Interceptor { chain ->
             chain.proceed(chain.request().newBuilder()
                     .header("Accept", "application/vnd.github.v3+json")
-                    .build())})
-
+                    .build())
+        }
     }
+
     @Provides
     @Singleton
-    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    fun providesConverter(): Converter.Factory {
+        val contentType = MediaType.parse("application/json")!!
+        val json = JSON.nonstrict
+        return stringBased(contentType, json::parse, json::stringify)
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(okHttpClient: OkHttpClient, converter: Converter.Factory): Retrofit {
         return Retrofit.Builder()
                 .baseUrl("https://api.github.com")
                 .client(okHttpClient)
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(converter)
                 .build()
     }
+
     @Provides
     @Singleton
     fun providesGithubApi(retrofit: Retrofit): GithubApi = retrofit.create(GithubApi::class.java)
 
-}
-private fun OkHttpClient.Builder.addInterceptors(interceptors:List<Interceptor>): OkHttpClient.Builder {
-    for (interceptor in interceptors) {
-        addInterceptor(interceptor)
-    }
-    return this
 }
