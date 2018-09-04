cff8fdca1fc130cdd9db7b7d170f95016582fdd4
m.suska <m.suska@partner.samsung.com>
2018-09-04 12:55:32 +0200

    Set title and subtitle in repository details

--- app/src/main/java/sandbox/myapplication/details/RepositoryDetailsFragment.kt
+++ app/src/main/java/sandbox/myapplication/details/RepositoryDetailsFragment.kt
@@ -2,6 +2,7 @@ package sandbox.myapplication.details
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,4 +14,13 @@ class RepositoryDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.repository_details, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        getSupportActionBar().apply {
+            title = repository.repoName
+            subtitle = repository.description
+        }
+    }
+
+    private fun getSupportActionBar() = (requireActivity() as AppCompatActivity).supportActionBar!!
 }
