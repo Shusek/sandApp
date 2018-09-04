4ba6b40b60f39991f715f6a1e28b8efe7c736d57
m.suska <m.suska@partner.samsung.com>
2018-09-04 12:57:43 +0200

    Revert "Set title and subtitle in repository details"
    This reverts commit cff8fdc because of ui problem after click back btn

--- app/src/main/java/sandbox/myapplication/details/RepositoryDetailsFragment.kt
+++ app/src/main/java/sandbox/myapplication/details/RepositoryDetailsFragment.kt
@@ -2,7 +2,6 @@ package sandbox.myapplication.details
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,13 +13,4 @@ class RepositoryDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.repository_details, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        getSupportActionBar().apply {
-            title = repository.repoName
-            subtitle = repository.description
-        }
-    }
-
-    private fun getSupportActionBar() = (requireActivity() as AppCompatActivity).supportActionBar!!
 }
