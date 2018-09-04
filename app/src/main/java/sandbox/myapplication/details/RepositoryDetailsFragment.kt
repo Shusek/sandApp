84fd323a1798c2637ace2c305a7daad5e0ab9d56
m.suska <m.suska@partner.samsung.com>
2018-09-04 18:24:17 +0200

    Add collapsing toolbar for repository detail

--- app/src/main/java/sandbox/myapplication/details/RepositoryDetailsFragment.kt
+++ app/src/main/java/sandbox/myapplication/details/RepositoryDetailsFragment.kt
@@ -1,16 +1,25 @@
 package sandbox.myapplication.details
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.repository_details.*
 import sampleapp.istari.myapplication.R
+import sandbox.myapplication.common.ToolbarFragment
 
-class RepositoryDetailsFragment : Fragment() {
+class RepositoryDetailsFragment : ToolbarFragment() {
     private val repository by lazy { RepositoryDetailsFragmentArgs.fromBundle(arguments).repository }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.repository_details, container, false)
+    }
+
+    override fun toolbarId() = R.id.detailToolbar
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getActionBar().title = repository.repoName
+        repoDetailsDescription.text = repository.description
     }
 }
