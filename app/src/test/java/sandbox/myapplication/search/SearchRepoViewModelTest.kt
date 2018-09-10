0f5e01b1f6f8a6eb7c3b93da11d3d12fd44ae282
m.suska <m.suska@partner.samsung.com>
2018-09-10 19:07:56 +0200

    Fix: assertion is not called when observer not receive any value

--- app/src/test/java/sandbox/myapplication/search/SearchRepoViewModelTest.kt
+++ app/src/test/java/sandbox/myapplication/search/SearchRepoViewModelTest.kt
@@ -1,11 +1,15 @@
 package sandbox.myapplication.search
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
+import android.arch.paging.PagedList
 import io.mockk.called
 import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Rule
 import org.junit.Test
 import sandbox.myapplication.common.RepositoryFactory
@@ -15,9 +19,10 @@ import sandbox.myapplication.search.repository.PagedSearchService
 class SearchRepoViewModelTest {
     @get:Rule
     val instantExecutor = InstantTaskExecutorRule()
-    val pagedSearchRepository = mockk<PagedSearchRepository>()
-    val service = PagedSearchService(pagedSearchRepository)
-    val viewModel = SearchRepoViewModel(service)
+    private val pagedSearchRepository = mockk<PagedSearchRepository>()
+    private val service = PagedSearchService(pagedSearchRepository)
+    private val viewModel = SearchRepoViewModel(service)
+
     @Test
     fun shouldSearchReturnProperCountOfLoadedItems() {
         val expectedRepositories = listOf(
@@ -25,20 +30,19 @@ class SearchRepoViewModelTest {
                 RepositoryFactory.createRepository(2))
         coEvery { pagedSearchRepository.getRepositories(any(), any()) } returns expectedRepositories
 
+        val singleObserver = viewModel.repositoriesPagedList.observeSingle()
 
         viewModel.search("retrofit")
-        viewModel.repositoriesPagedList.observeForever {
-            assertEquals(expectedRepositories, it!!)
-        }
+
+        assertEquals(expectedRepositories, singleObserver.value)
     }
 
     @Test
     fun shouldSearchReturnEmptyListWhenQueryIsEmptyWithoutCallRepository() {
+        val singleObserver = viewModel.repositoriesPagedList.observeSingle()
         viewModel.search("")
 
-        viewModel.repositoriesPagedList.observeForever {
-            assertEquals(0, it!!.size)
-        }
+        assertEquals(emptyList<PagedList<Repository>>(), singleObserver.value)
         verify {
             pagedSearchRepository wasNot called
         }
@@ -46,18 +50,36 @@ class SearchRepoViewModelTest {
 
     @Test
     fun shouldLoadDataFromMultiplePages() {
-        val firstPage = (1..100).map { RepositoryFactory.createRepository(it.toLong()) }
+        val firstPage = (1 until 100).map { RepositoryFactory.createRepository(it.toLong()) }
         val secondPage = (100..150).map { RepositoryFactory.createRepository(it.toLong()) }
 
         coEvery { pagedSearchRepository.getRepositories(eq(1), any()) } returns firstPage
         coEvery { pagedSearchRepository.getRepositories(eq(2), any()) } returns secondPage
 
-        val liveData = service.search("abc")
-        liveData.observeForever {
-            val expectedAllItems = firstPage + secondPage
-            it!!.loadAround(45)
-            assertEquals(expectedAllItems, it)
-        }
+        val singleObserver = viewModel.repositoriesPagedList.observeSingle()
+        viewModel.search("abc")
 
+        val expectedAllItems = firstPage + secondPage
+
+        val pagedList = singleObserver.value!!
+        assertEquals(firstPage, pagedList)
+        //load second page
+        pagedList.loadAround(45)
+        assertEquals(expectedAllItems, singleObserver.value)
+    }
+
+    private fun LiveData<PagedList<Repository>>.observeSingle() =
+            SingleObserver<PagedList<Repository>>()
+                    .apply { observeForever(this) }
+
+
+    private class SingleObserver<T> : Observer<T> {
+        var value: T? = null
+        override fun onChanged(newValue: T?) {
+            if (value != null) {
+                fail("This observer can contain only one value, old value = $value, new value = $newValue ")
+            }
+            this.value = newValue
+        }
     }
 }
